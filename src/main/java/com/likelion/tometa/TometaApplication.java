cc279d48890a3044d3fbf681de5ecb448589cf86
subin <ace10433@gmail.com>
2026-08-08 14:13:37 +0900

    chore: first commit

---
+++ src/main/java/com/likelion/tometa/TometaApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.tometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TometaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/tometa/TometaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.tometa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TometaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
