d2b11978ee04815f8554d80853f7b38395f4f2e8
yim0327 <yim020327@gmail.com>
2026-08-19 19:36:01 +0900

    feat(cosmetic): 화장품 검색 및 검색 결과 캐시 구현

--- src/main/java/com/likelion/tometa/global/config/s3/S3Config.java
+++ src/main/java/com/likelion/tometa/global/config/s3/S3Config.java
@@ -1,4 +1,4 @@
-package com.likelion.tometa.global.config;
+package com.likelion.tometa.global.config.s3;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
