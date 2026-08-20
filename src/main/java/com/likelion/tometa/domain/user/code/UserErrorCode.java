7777c9eb4aaa6dbeb341c7f7386776fe66b61880
tnqlsdl123 <ace10433@gmail.com>
2026-08-20 22:27:39 +0900

    feat(mypage): 알림 설정 수정 API 구현

--- src/main/java/com/likelion/tometa/domain/user/code/UserErrorCode.java
+++ src/main/java/com/likelion/tometa/domain/user/code/UserErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum UserErrorCode implements BaseErrorCode {
 
+    INVALID_NOTIFICATION_TIME(HttpStatus.BAD_REQUEST, "USER_4002", "알림 시간은 HH:mm 형식으로 입력해주세요."),
     INVALID_ANONYMOUS_SESSION(HttpStatus.UNAUTHORIZED, "USER_4011", "유효하지 않거나 만료된 사용자 세션입니다.");
 
     private final HttpStatus status;
